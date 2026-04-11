e0d0cfd071037d28acbbe83b2a2c65789c743c8b
Karan Shah <42066323+karanshah229@users.noreply.github.com>
2026-04-11 23:45:19 +0530

    Add: buy-and-sell-crypto - submission-12

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-12.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int result = 0;
+
+        int l = Integer.MAX_VALUE;
+        int r = 0;
+        
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] > prices[i-1]){
+                if(prices[i] > r){
+                    r = prices[i];
+                }
+                if(prices[i-1] < l){
+                    l = prices[i-1];
+                    r = prices[i];
+                    result = Math.max(result, r-l);
+                }
+
+                System.out.println(i + ":" + l + ":" + r + ":" + result);
+            }
+        }
+
+        result = Math.max(result, r-l);
+
+        return result;
+    }
+}
