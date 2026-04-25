46d6006a40508960e5c89691e3ab71acaab1aa22
Karan Shah <42066323+karanshah229@users.noreply.github.com>
2026-04-25 18:53:31 +0530

    Add: sliding-window-maximum - submission-13

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-13.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int resLen = nums.length - k + 1;
+        int[] result = new int[resLen];
+        int resIndex = 0;
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        // add first k elements to max heap
+        for(int i = 0; i < k; i++){
+            maxHeap.add(nums[i]);
+            hm.put(nums[i], i);
+        }
+        result[resIndex++] = maxHeap.peek();
+
+        // then remove first element and add new element
+        for(int i = k; i < nums.length; i++){
+            // instead of removing, let's peek and check if the element
+            // exists in the current range
+            // if not, keep polling to remove the max element - update the hashmap
+            // till you reach an element present in the current range
+            
+            maxHeap.add(nums[i]);
+            hm.put(nums[i], i);
+
+            int max = maxHeap.peek();
+            while(hm.get(max) <= i - k){
+                maxHeap.poll();
+                max = maxHeap.peek();
+            }
+
+            result[resIndex++] = max;
+        }
+
+        return result;
+    }
+}
