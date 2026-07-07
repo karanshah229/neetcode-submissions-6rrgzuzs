ebe48e53fbb86ed91d746c59b76a8e4a0920eb09
Karan Shah <42066323+karanshah229@users.noreply.github.com>
2026-07-07 15:22:44 +0530

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq = new int[26];
+
+        for(char task: tasks){
+            freq[task - 'A']++;
+        }
+
+        int maxFreq = Integer.MIN_VALUE;
+        int numMax = Integer.MIN_VALUE;
+        for(int i = 0; i < 26; i++){
+            if(freq[i] > maxFreq){
+                maxFreq = freq[i];
+                numMax = 1;
+            } else if(freq[i] == maxFreq){
+                numMax++;
+            }
+        }
+
+        return Math.max(tasks.length, ( (maxFreq -1) * (n+1) + numMax));
+    }
+}
