0a286c988837d261656cf648313655e120eea3c7
Karan Shah <42066323+karanshah229@users.noreply.github.com>
2026-07-20 18:10:31 +0530

    Add: permutations-ii - submission-3

---
+++ Data Structures & Algorithms/permutations-ii/submission-3.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void perm(List<List<Integer>> res, Map<Integer, Integer> hm, List<Integer> curr){
+        if(hm.size() == 0) {
+            res.add(curr);
+            return;
+        }
+
+        for(Map.Entry<Integer, Integer> entry: hm.entrySet()){
+            Map<Integer, Integer> hmC = new HashMap<>(hm);
+            List<Integer> currC = new ArrayList<>(curr);
+
+            int key = entry.getKey();
+            int val = entry.getValue();
+            currC.add(key);
+            if(val-1 == 0) hmC.remove(key);
+            else hmC.put(key, val-1);
+
+            perm(res, hmC, currC);
+        }
+    }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int n: nums) hm.put(n, hm.getOrDefault(n, 0) + 1);
+
+        List<List<Integer>> res = new ArrayList<>();
+        perm(res, hm, new ArrayList<>());
+
+        return res;
+    }
+}
