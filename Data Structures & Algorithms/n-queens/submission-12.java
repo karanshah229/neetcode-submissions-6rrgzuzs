97d8b350b1d827589252c5f3a6e5b02d25536c03
Karan Shah <42066323+karanshah229@users.noreply.github.com>
2026-07-23 11:33:53 +0530

    Add: n-queens - submission-12

---
+++ Data Structures & Algorithms/n-queens/submission-12.java
@@ -0,0 +1,83 @@
+class Solution {
+    int n;
+    record Point(int x, int y) {}
+
+    public boolean isValidPos(List<Point> curr, Point p){
+        for(Point o: curr){
+            if(o.x == p.x || o.y == p.y) return false;
+            if(Math.abs(o.x - p.x) == Math.abs(o.y - p.y)) return false;
+        }
+
+        return true;
+    }
+
+    public void solve(
+        List<List<Point>> pos,
+        List<Point> curr,
+        int row,
+        Set<Integer> usedCols,
+        Set<Integer> usedDiag1,
+        Set<Integer> usedDiag2
+    ) {
+        if (row == n) {
+            pos.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (
+                usedCols.contains(col) ||
+                usedDiag1.contains(row - col) ||
+                usedDiag2.contains(row + col)
+            ) {
+                continue;
+            }
+
+            curr.add(new Point(row, col));
+            usedCols.add(col);
+            usedDiag1.add(row - col);
+            usedDiag2.add(row + col);
+
+            solve(pos, curr, row + 1, usedCols, usedDiag1, usedDiag2);
+
+            curr.remove(curr.size() - 1);
+            usedCols.remove(col);
+            usedDiag1.remove(row - col);
+            usedDiag2.remove(row + col);
+        }
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        this.n = n;
+        List<List<Point>> pos = new ArrayList<>();
+        solve(
+            pos,
+            new ArrayList<>(),
+            0,
+            new HashSet<>(),
+            new HashSet<>(),
+            new HashSet<>()
+        );
+
+        System.out.println(pos);
+
+        List<List<String>> res = new ArrayList<>();
+        for(List<Point> plc: pos){
+            List<String> pStrL = new ArrayList<>();
+            for(int i = 0; i < n; i++){
+                String row = "";
+                Point p = plc.get(i);
+                for(int j = 0; j < n; j++){
+                    if(p.x == i && p.y == j) row += "Q";
+                    else row += ".";
+                }
+                pStrL.add(row);
+            }
+            res.add(pStrL);
+        }
+
+        System.out.println(res);
+
+        return res;
+    }
+}
